cf91a28f2d9fbe4dce1afcc157c4db067b4bf941
E99p1ant <i@github.red>
2023-04-07 01:49:28 +0800

    fix vue-pagination-tw.vue

--- src/components/vue-pagination-tw.vue
+++ src/components/vue-pagination-tw.vue
@@ -106,83 +106,47 @@ const calculatedPages = computed(() => {
 <template>
   <div :class="{ 'flex justify-between items-center': goButton }">
     <div :class="{ 'flex justify-between items-center': (props.styled === 'simple') }">
-      <nav
-        class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
-        aria-label="Pagination"
-        :class="{ 'mb-3': props.styled === 'centered' }"
-      >
-        <a
-          href="#"
+      <nav class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination"
+        :class="{ 'mb-3': props.styled === 'centered' }">
+        <a href="#"
           class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
           @click.prevent="changePage(currentPage - 1)"
-          :class="{ 'cursor-not-allowed bg-gray-200 hover:bg-gray-300': isFirstPage }"
-        >
+          :class="{ 'cursor-not-allowed bg-gray-200 hover:bg-gray-300': isFirstPage }">
           <span class="sr-only">Previous</span>
-          <svg
-            class="h-5 w-5"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
+          <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+            aria-hidden="true">
+            <path fill-rule="evenodd"
               d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-              clip-rule="evenodd"
-            />
+              clip-rule="evenodd" />
           </svg>
         </a>
-        <a
-          href="#"
-          aria-current="page"
+        <a href="#" aria-current="page"
           class="bg-white border border-gray-300 text-gray-500 relative inline-flex items-center px-4 py-2 text-sm font-medium"
-          @click.prevent="changePage(1)"
-          v-if="hasFirst()"
-        >1</a>
+          @click.prevent="changePage(1)" v-if="hasFirst()">1</a>
         <span
           class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700"
-          v-if="hasFirst()"
-        >...</span>
-        <a
-          href="#"
-          aria-current="page"
-          v-for="(page, index) in calculatedPages"
-          :key="index"
+          v-if="hasFirst()">...</span>
+        <a href="#" aria-current="page" v-for="(page, index) in calculatedPages" :key="index"
           class="bg-white border border-gray-300 text-gray-500 relative inline-flex items-center px-4 py-2 text-sm font-medium"
           :class="{ [`z-10 bg-indigo-50 !${borderActiveColor} !${borderTextActiveColor}`]: currentPage == page }"
-          @click.prevent="changePage(page)"
-        >{{ page }}</a>
+          @click.prevent="changePage(page)">{{ page }}</a>
         <!-- :class="currentPage === page ? `z-10 bg-indigo-50 !${borderActiveColor} !${borderTextActiveColor}` : '' }" -->
         <span
           class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700"
-          v-if="hasLast()"
-        >...</span>
-        <a
-          href="#"
-          aria-current="page"
+          v-if="hasLast()">...</span>
+        <a href="#" aria-current="page"
           class="bg-white border border-gray-300 text-gray-500 relative inline-flex items-center px-4 py-2 text-sm font-medium"
-          v-if="hasLast()"
-          @click.prevent="changePage(totalPages.value)"
-        >{{ totalPages.value }}</a>
-        <a
-          href="#"
+          v-if="hasLast()" @click.prevent="changePage(totalPages)">{{ totalPages }}</a>
+        <a href="#"
           class="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
           @click.prevent="changePage(currentPage + 1)"
-          :class="{ 'cursor-not-allowed bg-gray-200 hover:bg-gray-300': isLastPage }"
-        >
+          :class="{ 'cursor-not-allowed bg-gray-200 hover:bg-gray-300': isLastPage }">
           <span class="sr-only">Next</span>
-          <svg
-            class="h-5 w-5"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
+          <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+            aria-hidden="true">
+            <path fill-rule="evenodd"
               d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-              clip-rule="evenodd"
-            />
+              clip-rule="evenodd" />
           </svg>
         </a>
       </nav>
@@ -199,21 +163,12 @@ const calculatedPages = computed(() => {
       </div>
     </div>
     <div v-if="goButton">
-      <label
-        for="search"
-        class="pr-3 text-gray-500 text-xs font-semibold uppercase"
-      >{{ textBeforeInput }}</label>
-      <input
-        type="text"
-        placeholder="Page no"
+      <label for="search" class="pr-3 text-gray-500 text-xs font-semibold uppercase">{{ textBeforeInput }}</label>
+      <input type="text" placeholder="Page no"
         class="w-28 px-4 py-1 rounded-l outline-none border-t-2 border-l-2 border-b-2 placeholder:text-xs uppercase font-semibold"
-        :class="`focus:${borderActiveColor}`"
-        v-model="pageInput"
-      />
-      <button
-        class="px-4 py-1.5 rounded-r bg-indigo-500 text-white"
-        @click.prevent="changePage(pageInput)"
-      >{{ textAfterInput }}</button>
+        :class="`focus:${borderActiveColor}`" v-model="pageInput" />
+      <button class="px-4 py-1.5 rounded-r bg-indigo-500 text-white" @click.prevent="changePage(pageInput)">{{
+        textAfterInput }}</button>
     </div>
   </div>
 </template>
